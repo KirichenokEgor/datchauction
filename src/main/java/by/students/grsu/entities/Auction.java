c1f175a640d96290fdc419d1d76562f802aed90e
KirichenokEgor <Yahor_Kirychonak@epam.com>
2019-07-22 16:16:08 +0300

    Added some code in class prototypes

--- src/main/java/by/students/grsu/entities/Auction.java
+++ src/main/java/by/students/grsu/entities/Auction.java
@@ -2,6 +2,35 @@ package by.students.grsu.entities;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//аукционы происходят каждые 2 часа: от 0:00 до 22:00
+//enum AUCTION_TIME {AUCTIONat0,AUCTIONat2,AUCTIONat4,AUCTIONat6,AUCTIONat8,AUCTIONat12,AUCTIONat14,AUCTIONat16,AUCTIONat18,AUCTIONat20,AUCTIONat22}
+
 @Component
 public class Auction {
+    List lots;
+    //start time, end time? mb only start time (fixed duration)? or using Enum?
+
+    Auction(){
+        lots = new ArrayList<Lot>();
+    }
+
+    Auction(ArrayList<Lot> lots){
+        this.lots = (ArrayList)lots.clone();
+    }
+
+    void iteration(){
+        Lot lot;
+        //тут либо делать проверку на статус лота, либо сразу убирать купленные
+        for(int i = 0; i < lots.size(); i++){
+            lot = (Lot)lots.get(i);
+            if(lot.getPrice() == lot.getMin_price()) return;
+            lot.setPrice(lot.getPrice() - lot.getStep());
+        }
+    }
+
+
+
 }
--- src/main/java/by/students/grsu/entities/Lot.java
+++ src/main/java/by/students/grsu/entities/Lot.java
@@ -2,17 +2,72 @@ package by.students.grsu.entities;
 
 import org.springframework.stereotype.Component;
 
-//аукционы происходят каждые 2 часа: от 0:00 до 22:00
-enum AUCTION_TIME {AUCTIONat0,AUCTIONat2,AUCTIONat4,AUCTIONat6,AUCTIONat8,AUCTIONat12,AUCTIONat14,AUCTIONat16,AUCTIONat18,AUCTIONat20,AUCTIONat22}
-
 enum STATUS {ON_SELL,SOLD,WAITING}
 
 @Component
 public class Lot {
-    String ID;
-    Double price;
-    Double min_price;
-    String description;
+//    сделать id цифрой и через статик задавать наименьший свободный или взять имя(проблема неуникальности имени)
+    private String ID;
+    private Double price;
+    private Double min_price;
+    private String description;
+    private Double step;
     //поставить аукцион не здесь, а добавлять лоты в аукцион
+    private STATUS status;
 
+    final static int TIMES = 24;// 2 hours auction, every 5 minutes
+
+    Lot(String id, Double price, Double min_price, String description){
+        ID = id;
+        this.price = price;
+        this.min_price = min_price;
+        this.description = description;
+        step = (price - min_price)/TIMES;
+        status = STATUS.WAITING;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    //mb delete
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getMin_price() {
+        return min_price;
+    }
+
+    public void setMin_price(Double min_price) {
+        this.min_price = min_price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public STATUS getStatus() {
+        return status;
+    }
+
+    public void setStatus(STATUS status) {
+        this.status = status;
+    }
+
+    public Double getStep() {
+        return step;
+    }
 }
