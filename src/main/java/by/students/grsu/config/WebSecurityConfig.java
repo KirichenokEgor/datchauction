1727fd67d00dd245db8714d471f22aaf78c2289e
Aliaksei_Kunets <logicandpaws@gmail.com>
2019-09-09 18:46:20 +0300

    Main color theme added. Login/registration fixed. Few views re-styled

--- src/main/java/by/students/grsu/config/WebSecurityConfig.java
+++ src/main/java/by/students/grsu/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -42,7 +43,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         //System.out.println("handler bean created");
         return new CustomAccessDeniedHandler();
     }
-
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/css/**");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
